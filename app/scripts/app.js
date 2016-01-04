9a52bd6bc9fb87b5cef10ce3c2d5544e69c33e95
Maximiliano Poggio <maxenz@gmail.com>
2016-01-04 19:10:11 -0300

    Listo login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,30 +1,51 @@
 angular
-  .module('themesApp', [
-    'theme',
-    'theme.demos',
-  ])
-  .config(['$provide', '$routeProvider', function($provide, $routeProvider) {
-    'use strict';
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/index.html',
-        resolve: {
-          loadCalendar: ['$ocLazyLoad', function($ocLazyLoad) {
-            return $ocLazyLoad.load([
-              'bower_components/fullcalendar/fullcalendar.js',
-            ]);
-          }]
-        }
-      })
-      .when('/:templateFile', {
-        templateUrl: function(param) {
-          return 'views/' + param.templateFile + '.html';
-        }
-      })
-      .when('#', {
-        templateUrl: 'views/index.html',
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+.module('themesApp', [
+  'theme',
+  'theme.demos',
+])
+.constant('shamanConfiguration', {
+  'url': 'http://localhost/Shaman.Web/',
+  'port': '80'
+})
+.config(['$provide', '$routeProvider', function($provide, $routeProvider) {
+  'use strict';
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/index.html',
+    resolve: {
+      loadCalendar: ['$ocLazyLoad', function($ocLazyLoad) {
+        return $ocLazyLoad.load([
+          'bower_components/fullcalendar/fullcalendar.js',
+        ]);
+      }]
+    }
+  })
+  .when('/:templateFile', {
+    templateUrl: function(param) {
+      return 'views/' + param.templateFile + '.html';
+    }
+  })
+  .when('#', {
+    templateUrl: 'views/index.html',
+  })
+  .otherwise({
+    redirectTo: '/login'
+  });
+}])
+.run(['$rootScope', '$location', '$cookieStore', '$http',
+function($rootScope, $location, $cookieStore, $http){
+  'use strict';
+ $http.defaults.headers.post['Content-Type'] = 'application/json';
+
+  $rootScope.globals = $cookieStore.get('globals') || {};
+  if ($rootScope.globals.currentUser) {
+    $http.defaults.headers.common['Authorization'] = 'Basic' + $rootScope.globals.currentUser.authdata;
+  }
+
+  $rootScope.$on('$locationChangeStart', function(event, next, current){
+    if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+      $location.path('/login');
+    }
+  });
+
+}]);
--- app/scripts/core/controllers/loginController.js
+++ app/scripts/core/controllers/loginController.js
@@ -1,16 +1,41 @@
 angular
-  .module('theme.core.login_controller', [])
-  .controller('LoginController', LoginController);
+.module('theme.core.login_controller', [])
+.controller('LoginController', LoginController);
 
-  LoginController.$inject = ['$scope', '$filter', '$theme'];
-   function LoginController($scope, $filter, $theme) {
+LoginController.$inject = ['$scope', '$filter', '$theme', '$rootScope', '$location', 'AuthenticationService'];
+function LoginController($scope, $filter, $theme, $rootScope, $location, AuthenticationService) {
+  'use strict';
 
-    'use strict';
-    
-    $theme.set('fullscreen', true);
+  $scope.data = {};
+  $scope.login = login;
 
-    $scope.$on('$destroy', function() {
-      $theme.set('fullscreen', false);
+  // --> Seteo de temas
+  $theme.set('fullscreen', true);
+
+  $scope.$on('$destroy', function() {
+    $theme.set('fullscreen', false);
+  });
+
+  AuthenticationService.clearCredentials();
+
+  function login() {
+    $scope.data.dataLoading = true;
+    AuthenticationService.login($scope.data.username, $scope.data.password)
+    .then(function(data){
+      AuthenticationService.setCredentials($scope.data.username, $scope.data.password);
+      $location.path('/');
+    }, function(error){
+      switch (error.status) {
+        case 401:
+        $scope.data.error = 'Los datos de inicio de sesión son inválidos';
+        break;
+        case 500:
+        $scope.data.error = 'Error en el servidor. Intente ingresar luego';
+        break;
+      }
+      $scope.data.dataLoading = false;
     });
 
   }
+
+}
